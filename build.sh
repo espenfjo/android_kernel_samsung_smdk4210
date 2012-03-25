4a88d046445b2498d08d7e052022c7b186973851
Espen Fjellvær Olsen <espen@mrfjo.org>
2012-03-25 20:54:24 +0200

    Add correct cross compile prefix path on OSX, make CROSS_PREFIX availble to pre set environment
    Id: d6679601ac0de14667450af912def5dd6c774b1e

--- build.sh
+++ build.sh
@@ -24,7 +24,17 @@ setup ()
     fi
 
     export USE_SEC_FIPS_MODE=true
-    CROSS_PREFIX="$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-"
+# Common defines (Arch-dependent)
+    case `uname -s` in
+        Darwin)
+            CROSS_PREFIX_ARCH="darwin-x86"
+            ;;
+        *)
+            CROSS_PREFIX_ARCH="linux-x86"
+            ;;
+    esac
+    
+    CROSS_PREFIX=${CROSS_PREFIX:-"$ANDROID_BUILD_TOP/prebuilt/$CROSS_PREFIX_ARCH/toolchain/arm-eabi-4.4.3/bin/arm-eabi-"}    
 }
 
 build ()
@@ -36,7 +46,7 @@ build ()
     [ x = "x$NO_RM" ] && rm -fr "$target_dir"
     mkdir -p "$target_dir/usr"
     cp "$KERNEL_DIR/usr/"*.list "$target_dir/usr"
-    sed "s|usr/|$KERNEL_DIR/usr/|g" -i "$target_dir/usr/"*.list
+    sed -i' ' "s|usr/|$KERNEL_DIR/usr/|g" "$target_dir/usr/"*.list
     [ x = "x$NO_DEFCONFIG" ] && mka -C "$KERNEL_DIR" O="$target_dir" android_${target}_defconfig ARCH=arm HOSTCC="$CCACHE gcc"
     if [ x = "x$NO_BUILD" ] ; then
         mka -C "$KERNEL_DIR" O="$target_dir" ARCH=arm HOSTCC="$CCACHE gcc" CROSS_COMPILE="$CCACHE $CROSS_PREFIX" modules
